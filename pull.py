8d1c643a33e1af3bceb31e0789e41dda91c52181
Jonathan Sims <jonathan.sims@pm.me>
2020-09-28 01:11:41 +0000

    [WORKING] pull from newsapi and put to s3 as json

--- pull.py
+++ pull.py
@@ -1,6 +1,6 @@
 import logging
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 import io
 import boto3
 import random
@@ -11,18 +11,24 @@ from newsapi import NewsApiClient
 NEWSAPI_KEY = os.environ['NEWSAPI_KEY']
 newsapi = NewsApiClient(api_key=NEWSAPI_KEY)
 
+# Initialize S3 Bucket
+S3BUCKETNAME = os.environ['S3BUCKETNAME']
+s3 = boto3.client('s3')
+
 # Try to pull parameters from environment (for container usage)
-# Else define defaults here
+# Else define defaults 
 if all(param in ['STARTPULL', 'ENDPULL', 'QUERYPULL'] for param in os.environ) is True:
     STARTPULL = os.environ['STARTPULL']
     ENDPULL = os.environ['ENDPULL']
     QUERYPULL = os.environ['QUERYPULL']
 else:
-    STARTPULL = datetime.today().strftime('%Y-%m-%d')
+    print(f'WARNING: Job params not all defined in environment. Running defaults')
+    yesterday = datetime.today() + timedelta(days=-1)
+    STARTPULL = yesterday.strftime('%Y-%m-%d')
     ENDPULL = STARTPULL
     QUERYPULL = 'cryptocurrency'
 
-# Get top 10 pages of results by  relevancy
+# Get up to 10 pages of results by relevancy
 for PAGEPULL in range(1,10):
     news_pull = newsapi.get_everything(q=QUERYPULL,
                                        from_param=STARTPULL,
@@ -30,12 +36,20 @@ for PAGEPULL in range(1,10):
                                        language='en',
                                        sort_by='relevancy',
                                        page=PAGEPULL)
+
+    # Put JSON as string to S3
+    json_text = json.dumps(news_pull)
+    s3.put_object(Bucket=S3BUCKETNAME,
+                  Key=f'{STARTPULL}-{QUERYPULL}-page-{PAGEPULL}.json', 
+                  Body=json_text)
+
+    # Break when no articles left
     if news_pull['articles'] == []:
         break
 
+# Logging
 logging.exception('')
+print(f'Start pull: {STARTPULL}')
+print(f'End pull: {ENDPULL}')
+print(f'Query term: {QUERYPULL}')
 print(f'Stopped on page: {PAGEPULL}')
-print(news_pull)
-
-# Put JSON as string to S3
-s3 = boto3.resource('s3')
