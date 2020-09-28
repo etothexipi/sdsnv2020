4976822fedda9eed91f29a92b122d637cf89c8de
Jonathan Sims <jonathan.sims@pm.me>
2020-09-28 00:21:45 +0000

    [REFACTOR] change pipeline example to use newsapi-python lib

--- pull.py
+++ pull.py
@@ -1,81 +1,41 @@
-import newspaper
 import logging
+import json
+from datetime import datetime
 import io
 import boto3
-import pandas as pd
-import pickle
-import nltk
 import random
+import os
+from newsapi import NewsApiClient
 
-nltk.download('punkt')
+# Initialize NewsApi Client with Key
+NEWSAPI_KEY = os.environ['NEWSAPI_KEY']
+newsapi = NewsApiClient(api_key=NEWSAPI_KEY)
+
+# Try to pull parameters from environment (for container usage)
+# Else define defaults here
+if all(param in ['STARTPULL', 'ENDPULL', 'QUERYPULL'] for param in os.environ) is True:
+    STARTPULL = os.environ['STARTPULL']
+    ENDPULL = os.environ['ENDPULL']
+    QUERYPULL = os.environ['QUERYPULL']
+else:
+    STARTPULL = datetime.today().strftime('%Y-%m-%d')
+    ENDPULL = STARTPULL
+    QUERYPULL = 'cryptocurrency'
+
+# Get top 10 pages of results by  relevancy
+for PAGEPULL in range(1,10):
+    news_pull = newsapi.get_everything(q=QUERYPULL,
+                                       from_param=STARTPULL,
+                                       to=ENDPULL,
+                                       language='en',
+                                       sort_by='relevancy',
+                                       page=PAGEPULL)
+    if news_pull['articles'] == []:
+        break
+
+logging.exception('')
+print(f'Stopped on page: {PAGEPULL}')
+print(news_pull)
+
+# Put JSON as string to S3
 s3 = boto3.resource('s3')
-
-# Pick random top popular RL and save result
-# We need to avoid article/view limits on news sites
-top_urls = newspaper.popular_urls()
-randtop2 = random.sample(top_urls,2)
-
-for url in randtop2:
-    
-    cnt = 0
-
-    for article in newspaper.build(url).articles[:3]:
-
-        article.download()
-        article.parse()
-        article.nlp()
-
-        if url.find('www.') == -1 and url.find('.com') == 1:
-            site = url.split('://')[1]
-            site = site.split('.com')[0]
-        elif url.find('www.') > -1 and url.find('.com') ==1:    
-            site = url.split('://www.')[1]
-            site = site.split('.com')[0]
-            print(site)
-        else:
-            logging.exception('')
-            break
-
-        data = [article.keywords, article.summary]
-
-        pickle_fname = f'newspaper/raw/{site}.{cnt}.pkl'
-        pickle_buffer = io.BytesIO()
-        #s3_obj = s3.Object(bucket_name='s3://sdsnv2020-etothexipi-intro', key=pickle_fname)
-
-        logging.exception('')
-
-        with open(pickle_buffer, 'wb') as p:
-            pickle.dump(data, p)
-            s3.Object(bucket_name='s3://sdsnv2020-etothexipi-intro', key=pickle_fname).put(Body=pickle_buffer.getvalue())
-            logging.exception('')
-
-        cnt += 1
-
-    logging.exception('')
-
-
-
-'''
-build = newspaper.build('http://www.technologyreview.com')
-articles = build.articles
-
-for article in build.articles[:10]:
-    article.download()
-    article.parse()
-    article.nlp()
-
-    with open(f'./{site}.{y}.pkl', 'wb') as p:
-        pickle.dump([article.keywords, article.summary], p)
-
-    y += 1
-
-with open('./build.0.pkl', 'wb') as p:
-    pickle.dump(articles, p)
-
-for article in theonion.articles[:30]:
-    url = article.url
-    print(url)
-    article = newspaper.Article(url)
-    article.download()
-    article.parse()
-'''
